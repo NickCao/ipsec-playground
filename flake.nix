45d9649ded05b3a099e6bd418ac31ba2364b4d12
Nick Cao <nickcao@nichi.co>
2022-09-30 16:58:03 +0800

    add config

--- flake.nix
+++ flake.nix
@@ -6,6 +6,14 @@
         system = "x86_64-linux";
         modules = [
           ({ config, pkgs, lib, ... }: {
+            environment.variables.NFQ_TEST = toString (pkgs.rustPlatform.buildRustPackage {
+              name = "nfq-test";
+              src = ./nfq-test;
+              cargoLock.lockFile = ./nfq-test/Cargo.lock;
+            });
+            environment.systemPackages = with pkgs; [ cargo rustc ];
+            services.getty.autologinUser = "root";
+            users.users.test.isNormalUser = true;
           })
         ];
       };
