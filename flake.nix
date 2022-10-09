a5d643b54165d8c226bcbe42605ef3b58480e714
Nick Cao <nickcao@nichi.co>
2022-10-09 21:14:18 +0800

    create

--- flake.nix
+++ flake.nix
@@ -1,201 +1,103 @@
 {
   inputs.nixpkgs.url = "github:NickCao/nixpkgs/nixos-unstable-small";
-  outputs = { self, nixpkgs, ... }: {
+  outputs = { self, nixpkgs, ... }: rec {
+    packages.x86_64-linux.xfirm = nixpkgs.legacyPackages.x86_64-linux.buildGoModule {
+      name = "xfirm";
+      src = ./xfirm;
+      vendorHash = "sha256-3Qc2tNGUtb0nPqp7dKmwzJtncXgN3PGYdwPIX3AK4rg=";
+      overrideModAttrs = _: {
+        postConfigure = ''
+          export GOPROXY=https://goproxy.cn
+        '';
+      };
+    };
     packages.x86_64-linux.ipsec = nixpkgs.legacyPackages.x86_64-linux.nixosTest {
       name = "ipsec";
       nodes =
         let
-          nodes = {
-            node1 = {
-              id = 1;
-              addr = "192.168.1.1";
-              prefix = "fc00:1::1/64";
-              port = 50001;
-              pub = ''
-                -----BEGIN PUBLIC KEY-----
-                MCowBQYDK2VwAyEA0fIFaKz0kB/jlgjHQZLdlfELwUx5W3/mEErkDPRTKgg=
-                -----END PUBLIC KEY-----
-              '';
-              priv = ''
-                -----BEGIN PRIVATE KEY-----
-                MC4CAQAwBQYDK2VwBCIEIMamaugVRsYMY3N2iS5jaxDnnuUzFE6A2sg8dh7WStD1
-                -----END PRIVATE KEY-----
-              '';
-            };
-            node2 = {
-              id = 2;
-              addr = "192.168.1.2";
-              prefix = "fc00:2::1/64";
-              port = 50002;
-              pub = ''
-                -----BEGIN PUBLIC KEY-----
-                MCowBQYDK2VwAyEAqLIvzUm/xMgSyDW3EmtOw65zjPuLsN7Pz57fFJiOCsg=
-                -----END PUBLIC KEY-----
-              '';
-              priv = ''
-                -----BEGIN PRIVATE KEY-----
-                MC4CAQAwBQYDK2VwBCIEIBujPwQglT7ZgM7MBXM9SNXax5ClhEj3bysEdlFbt/nq
-                -----END PRIVATE KEY-----
-              '';
-            };
-            node3 = {
-              id = 3;
-              addr = "192.168.1.3";
-              prefix = "fc00:3::1/64";
-              port = 50003;
-              pub = ''
-                -----BEGIN PUBLIC KEY-----
-                MCowBQYDK2VwAyEArVLalM1amJ9neWgPb8ACmLUC8CgD/JvT09IlA3PvHDo=
-                -----END PUBLIC KEY-----
-              '';
-              priv = ''
-                -----BEGIN PRIVATE KEY-----
-                MC4CAQAwBQYDK2VwBCIEIMqyIbIcIWt09kAXfDm/XLbsSJQQykTgP2u3EiszHxgn
-                -----END PRIVATE KEY-----
-              '';
-            };
+          remotes = [
+            {
+              remote_addrs = [ "192.168.1.1" "%any" ];
+              remote_port = 500;
+              public_key = "MCowBQYDK2VwAyEA0fIFaKz0kB/jlgjHQZLdlfELwUx5W3/mEErkDPRTKgg=";
+              mtu = 1400;
+              name = "node1";
+            }
+            {
+              remote_addrs = [ "192.168.1.2" "%any" ];
+              remote_port = 500;
+              public_key = "MCowBQYDK2VwAyEAqLIvzUm/xMgSyDW3EmtOw65zjPuLsN7Pz57fFJiOCsg=";
+              mtu = 1400;
+              name = "node2";
+            }
+            {
+              remote_addrs = [ "192.168.1.3" "%any" ];
+              remote_port = 500;
+              public_key = "MCowBQYDK2VwAyEArVLalM1amJ9neWgPb8ACmLUC8CgD/JvT09IlA3PvHDo=";
+              mtu = 1400;
+              name = "node3";
+            }
+          ];
+          locals = {
+            node1 = [{
+              local_addrs = [ "%any" ];
+              local_port = 0;
+              private_key = "MC4CAQAwBQYDK2VwBCIEIMamaugVRsYMY3N2iS5jaxDnnuUzFE6A2sg8dh7WStD1";
+              mtu = 1400;
+              prefix = "node1";
+            }];
+            node2 = [{
+              local_addrs = [ "%any" ];
+              local_port = 0;
+              private_key = "MC4CAQAwBQYDK2VwBCIEIBujPwQglT7ZgM7MBXM9SNXax5ClhEj3bysEdlFbt/nq";
+              mtu = 1400;
+              prefix = "node2";
+            }];
+            node3 = [{
+              local_addrs = [ "%any" ];
+              local_port = 0;
+              private_key = "MC4CAQAwBQYDK2VwBCIEIMqyIbIcIWt09kAXfDm/XLbsSJQQykTgP2u3EiszHxgn";
+              mtu = 1400;
+              prefix = "node3";
+            }];
           };
+          mkConfig = name: (builtins.toFile "xfirm.conf" (builtins.toJSON {
+            inherit remotes;
+            locals = locals.${name};
+          }));
         in
         nixpkgs.lib.mapAttrs
-          (n: self:
-            let
-              others = nixpkgs.lib.filterAttrs (name: node: node.id != self.id) nodes;
-            in
-            ({ config, pkgs, ... }: {
-              boot.kernel.sysctl = {
-                "net.ipv6.conf.default.forwarding" = 1;
-                "net.ipv4.conf.default.forwarding" = 1;
-                "net.ipv6.conf.all.forwarding" = 1;
-                "net.ipv4.conf.all.forwarding" = 1;
-              };
-              environment.systemPackages = [ pkgs.strongswan pkgs.iperf3 pkgs.mtr ];
-              environment.etc."swanctl/private/local.pem".text = self.priv;
-              networking = {
-                firewall = {
-                  allowedUDPPorts = [ self.port ];
-                  trustedInterfaces = pkgs.lib.attrNames others;
-                };
-                useNetworkd = true;
-              };
-              services.iperf3.enable = true;
-              systemd.network.netdevs = pkgs.lib.mapAttrs
-                (name: node: {
-                  netdevConfig = {
-                    Kind = "xfrm";
-                    Name = name;
-                  };
-                  xfrmConfig = {
-                    InterfaceId = node.id;
-                    Independent = true;
-                  };
-                })
-                others // {
-                gravity = {
-                  netdevConfig = {
-                    Kind = "dummy";
-                    Name = "gravity";
-                  };
-                };
-              };
-              systemd.network.networks = pkgs.lib.mapAttrs
-                (name: node: {
-                  inherit name;
-                  linkConfig = {
-                    Multicast = true;
-                  };
-                })
-                others // {
-                gravity = {
-                  name = "gravity";
-                  address = [ self.prefix ];
-                };
-              };
-              services.strongswan-swanctl = {
-                enable = true;
-                strongswan.extraConfig = ''
-                  charon {
-                    port = 0
-                    port_nat_t = ${toString self.port}
-                  }
-                '';
-                swanctl = {
-                  connections = pkgs.lib.mapAttrs
-                    (name: node: {
-                      version = 2;
-                      encap = true;
-                      local_addrs = [ "%any" ]; # acccept connection to any address
-                      remote_addrs = [ node.addr "%any" ]; # try connection to specific address, allow connection from any address
-                      remote_port = node.port;
-                      if_id_out = toString node.id;
-                      if_id_in = toString node.id;
-                      local.default = {
-                        auth = "pubkey";
-                        pubkeys = [ (builtins.toFile "local.pub" self.pub) ];
-                      };
-                      remote.default = {
-                        auth = "pubkey";
-                        pubkeys = [ (builtins.toFile "remote.pub" node.pub) ];
-                      };
-                      children.default = {
-                        local_ts = [ "0.0.0.0/0" "::/0" ];
-                        remote_ts = [ "0.0.0.0/0" "::/0" ];
-                        start_action = "trap";
-                      };
-                    })
-                    others;
-                };
-              };
-              services.bird2 = {
-                enable = true;
-                config = ''
-                  protocol device {
-                    scan time 1;
-                  }
-
-                  protocol kernel {
-                    ipv6 {
-                      export all;
-                      import none;
-                    };
-                  }
-
-                  protocol direct {
-                    ipv6;
-                    interface "gravity";
-                  }
-
-                  protocol babel {
-                    ipv6 {
-                      export all;
-                      import all;
-                    };
-                    randomize router id;
-                    interface "node*" {
-                      hello interval 1 s;
-                    };
-                  }
-                '';
-              };
-            }))
-          nodes;
+          (name: cfg: ({ config, pkgs, ... }: {
+            boot.kernel.sysctl = {
+              "net.ipv6.conf.default.forwarding" = 1;
+              "net.ipv4.conf.default.forwarding" = 1;
+              "net.ipv6.conf.all.forwarding" = 1;
+              "net.ipv4.conf.all.forwarding" = 1;
+            };
+            environment.systemPackages = [ pkgs.strongswan pkgs.iperf3 pkgs.mtr ];
+            services.strongswan-swanctl.enable = true;
+            systemd.services.xfirm = {
+              path = [ pkgs.iproute2 ];
+              wantedBy = [ "multi-user.target" ];
+              after = [ "strongswan-swanctl.service" ];
+              serviceConfig.RemainAfterExit = true;
+              script = ''
+                ip link add link1 type xfrm dev lo if_id 0x1
+                ip link add link2 type xfrm dev lo if_id 0x2
+                ip link add link3 type xfrm dev lo if_id 0x3
+                ip link set link1 multicast on up
+                ip link set link2 multicast on up
+                ip link set link3 multicast on up
+                ${packages.x86_64-linux.xfirm}/bin/xfirm -config ${mkConfig name}
+              '';
+            };
+          }))
+          locals;
       testScript = ''
         start_all()
-        node1.wait_for_unit("strongswan-swanctl.service")
-        node1.wait_for_unit("bird2.service")
-        print(node1.succeed("ip a"))
-        node1.succeed("sleep 5")
-        print(node1.succeed("birdc s babel n"))
-        print(node1.succeed("birdc s r"))
-        print(node1.succeed("mtr -r fc00:2::1"))
-        node1.succeed("ip addr flush dev eth1")
-        node1.succeed("ip addr add 192.168.1.100/24 dev eth1")
-        node1.succeed("sleep 5")
-        print(node1.succeed("mtr -r fc00:2::1"))
-        node1.succeed("ip r add 192.168.1.2 dev lo")
+        node1.wait_for_unit("xfirm.service")
+        print(node1.succeed("swanctl --list-conns"))
         node1.succeed("sleep 10")
-        print(node1.succeed("birdc s r"))
-        print(node2.succeed("birdc s r"))
-        print(node1.succeed("mtr -r fc00:2::1"))
       '';
     };
   };
--- xfirm/config/config.go
+++ xfirm/config/config.go
@@ -1,22 +1,22 @@
 package config
 
 type Config struct {
-	Locals  []Local
-	Remotes []Remote
+	Locals  []Local  `json:"locals"`
+	Remotes []Remote `json:"remotes"`
 }
 
 type Local struct {
-	LocalAddrs []string
-	LocalPort  uint32
-	PrivateKey []byte
-	MTU        uint32
-	Prefix     string
+	LocalAddrs []string `json:"local_addrs"`
+	LocalPort  uint32   `json:"local_port"`
+	PrivateKey []byte   `json:"private_key"`
+	MTU        uint32   `json:"mtu"`
+	Prefix     string   `json:"prefix"`
 }
 
 type Remote struct {
-	RemoteAddrs []string
-	RemotePort  uint32
-	PublicKey   []byte
-	MTU         uint32
-	Name        string
+	RemoteAddrs []string `json:"remote_addrs"`
+	RemotePort  uint32   `json:"remote_port"`
+	PublicKey   []byte   `json:"public_key"`
+	MTU         uint32   `json:"mtu"`
+	Name        string   `json:"name"`
 }
