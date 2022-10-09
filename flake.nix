e1c80d2d20fd2df3d2f55262b6bdf14450da4dcd
Nick Cao <nickcao@nichi.co>
2022-10-09 09:24:41 +0800

    fix

--- flake.nix
+++ flake.nix
@@ -10,36 +10,6 @@
             node2 = { id = 2; addr = "192.168.1.2"; prefix = "100.64.2.0/24"; };
             node3 = { id = 3; addr = "192.168.1.3"; prefix = "100.64.3.0/24"; };
           };
-          secrets = {
-            ike = nixpkgs.lib.mapAttrs
-              (name: node: {
-                id.default = "${name}@gravity";
-                secret = name; # FIXME: use a real secret key
-              })
-              nodes;
-          };
-          connections = self: nixpkgs.lib.mapAttrs
-            (name: node: {
-              version = 2;
-              encap = true;
-              remote_addrs = [ node.addr ];
-              if_id_out = "1";
-              if_id_in = "1";
-              local.main = {
-                auth = "psk";
-                id = "${self}@gravity";
-              };
-              remote.main = {
-                auth = "psk";
-                id = "${name}@gravity";
-              };
-              children.default = {
-                local_ts = [ "0.0.0.0/0" "::/0" ];
-                remote_ts = [ "0.0.0.0/0" "::/0" ];
-                start_action = "start";
-              };
-            })
-            nodes;
         in
         nixpkgs.lib.mapAttrs
           (n: self:
@@ -69,20 +39,45 @@
                   inherit name;
                 })
                 others;
-              /*
               services.strongswan-swanctl = {
                 enable = true;
                 swanctl = {
-                  connections = connections "node1";
-                  inherit secrets;
+                  connections = pkgs.lib.mapAttrs
+                    (name: node: {
+                      version = 2;
+                      encap = true;
+                      remote_addrs = [ node.addr ];
+                      if_id_out = toString node.id;
+                      if_id_in = toString node.id;
+                      local.main = {
+                        auth = "psk";
+                        id = "${n}@gravity";
+                      };
+                      remote.main = {
+                        auth = "psk";
+                        id = "${name}@gravity";
+                      };
+                      children.default = {
+                        local_ts = [ "0.0.0.0/0" "::/0" ];
+                        remote_ts = [ "0.0.0.0/0" "::/0" ];
+                        start_action = "start";
+                      };
+                    })
+                    others;
+                  secrets.ike = pkgs.lib.mapAttrs
+                    (name: node: {
+                      id.default = "${name}@gravity";
+                      secret = name; # FIXME: use a real secret key
+                    })
+                    others;
                 };
               };
-              */
             }))
           nodes;
       testScript = ''
-        node1.wait_for_unit("network-online.target")
-        print(node1.succeed("ip a"))
+        start_all()
+        node1.wait_for_unit("strongswan-swanctl.service")
+        print(node1.succeed("swanctl --list-conns"))
       '';
     };
   };
