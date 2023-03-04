2e7ba99193a2b43e53168097110e5cf824fe89ab
Nick Cao <nickcao@nichi.co>
2023-03-05 00:28:06 +0800

    fix build

--- xfirm/xfirm.go
+++ xfirm/xfirm.go
@@ -3,6 +3,7 @@ package main
 import (
 	"crypto/ed25519"
 	"crypto/x509"
+	"encoding/base64"
 	"encoding/json"
 	"flag"
 	"fmt"
@@ -12,26 +13,49 @@ import (
 	"github.com/strongswan/govici/vici"
 )
 
-var registryFile = flag.String("registry", "registry.json", "path to registry")
-var id4 = flag.String("id4", "", "id for ipv4")
-var id6 = flag.String("id6", "", "id for ipv6")
+var registryPath = flag.String("registry", "registry.json", "path to registry")
+var configPath = flag.String("config", "config.json", "path to config")
 
 func main() {
 	flag.Parse()
 
-	file, err := os.Open(*registryFile)
+	// load registry
+	registryFile, err := os.Open(*registryPath)
 	if err != nil {
 		panic(err)
 	}
 
-	decoder := json.NewDecoder(file)
+	registryDecoder := json.NewDecoder(registryFile)
+
 	registry := config.Registy{}
-	err = decoder.Decode(&registry)
+
+	err = registryDecoder.Decode(&registry)
 	if err != nil {
 		panic(err)
 	}
 
-	sk, _, err := GenerateKeypair()
+	// load config
+	configFile, err := os.Open(*configPath)
+	if err != nil {
+		panic(err)
+	}
+
+	configDecoder := json.NewDecoder(configFile)
+
+	config := config.Config{}
+
+	err = configDecoder.Decode(&config)
+	if err != nil {
+		panic(err)
+	}
+
+	session, err := vici.NewSession()
+	if err != nil {
+		panic(err)
+	}
+
+	// using static key pair to ease testing
+	sk, err := base64.RawStdEncoding.DecodeString("MC4CAQAwBQYDK2VwBCIEIKA57upEiuTmii9iE8d79U5896A7uV9kC78f6fJwQMbx")
 	if err != nil {
 		panic(err)
 	}
@@ -41,46 +65,39 @@ func main() {
 		panic(err)
 	}
 
-	sess, err := vici.NewSession()
-	if err != nil {
-		panic(err)
-	}
-
 	key, err := vici.MarshalMessage(EncodePrivateKey(sk))
 	if err != nil {
 		panic(err)
 	}
 
-	_, err = sess.CommandRequest("load-key", key)
+	_, err = session.CommandRequest("load-key", key)
 	if err != nil {
 		panic(err)
 	}
 
-	for _, local := range []string{*id4, *id6} {
-		for _, remote := range registry {
-			conn, err := vici.MarshalMessage(NewConnection(
-				[]string{"%any"},
-				[]string{remote.Addr, "%any"},
-				500,
-				500,
-				pk,
-				pk,
-				local,
-				remote.Id,
-			))
-			if err != nil {
-				panic(err)
-			}
+	for _, local := range config.Endpoints {
+		for _, identity := range registry {
+			for _, remote := range identity.Endpoints {
+				conn, err := vici.MarshalMessage(NewConnection(
+					local,
+					remote,
+					pk,
+					pk,
+				))
+				if err != nil {
+					panic(err)
+				}
 
-			msg := vici.NewMessage()
-			err = msg.Set(fmt.Sprintf("%s-%s", local, remote.Id), conn)
-			if err != nil {
-				panic(err)
-			}
+				msg := vici.NewMessage()
+				err = msg.Set(fmt.Sprintf("%s-%s", local.Id, remote.Id), conn)
+				if err != nil {
+					panic(err)
+				}
 
-			_, err = sess.CommandRequest("load-conn", msg)
-			if err != nil {
-				panic(err)
+				_, err = session.CommandRequest("load-conn", msg)
+				if err != nil {
+					panic(err)
+				}
 			}
 		}
 	}
